a6e6e3d51f3747f9c768a42922097c9940a92925
karolrdg <karolinerodrigues12@outlook.com>
2026-09-22 14:54:38 -0300

    feat: implement user registration
    Adiciona controlador para propriedades rurais
    
    Foi adicionado o controlador `RuralPropertiesController` com dois endpoints:
    - `POST /api/RuralProperties`: Criação de uma nova propriedade rural.
    - `GET /api/RuralProperties/{id}`: Busca de uma propriedade rural pelo ID.
    
    Também foi criada a classe `CreateRuralPropertyRequest` como DTO para receber os dados necessários para criar propriedades rurais.
    
    Os namespaces necessários foram adicionados, e o atributo `[Authorize]` foi aplicado ao controlador para restringir o acesso a usuários autenticados.

---
+++ AgroIA.Api/Controllers/RuralPropertiesController.cs
@@ -0,0 +1,60 @@
+﻿using AgroIA.Api.Data;
+using AgroIA.Api.Domain.Entities;
+using AgroIA.Api.DTOs.RuralProperties;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgroIA.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class RuralPropertiesController : ControllerBase
+{
+    private readonly AgroIADbContext _context;
+
+    public RuralPropertiesController(AgroIADbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        CreateRuralPropertyRequest request)
+    {
+        var ruralProperty = new RuralProperty(
+            request.Name,
+            request.Location,
+            request.AreaInHectares,
+            request.OrganizationId);
+
+        _context.RuralProperties.Add(ruralProperty);
+
+        await _context.SaveChangesAsync();
+
+        //201 Created
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = ruralProperty.Id },
+            ruralProperty);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var ruralProperty = await _context.RuralProperties
+            .AsNoTracking()
+            .FirstOrDefaultAsync(property => property.Id == id);
+
+        // Se não encontrar, retorna 404
+        if (ruralProperty is null)
+        {
+            return NotFound(new
+            {
+                message = "Propriedade rural não encontrada."
+            });
+        }
+        return Ok(ruralProperty);
+    }
+}
